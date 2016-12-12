37bca4642a402f2c55c69119eb784d1b5b0d4394
Aguirre <ricardoaguiremorice@hotmail.com>
2016-12-11 21:34:13 -0600

    se creo commit

---
+++ Csharp/HolaMundo/app_holamundo/app_holamundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Soy un programa hecho visual strudio que usa Git");
+            System.Console.ReadLine();
+        }
+    }
+}
